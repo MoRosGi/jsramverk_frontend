f8e65bff620b65333f6fa1d8678fd15037269686
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-01 09:58:04 +0200

    Change useEffect Hook, add fetchData

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,29 @@ const Home = () => {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        try {
-        fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/')
-        // .then(res => res.json())
-        // .then(res => setMessage(res.description));
-        // .then(res => res);
-        } catch (e) {
-        console.error(e);
-        }
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/gets/66fa8ebaf7c5bf59b01b53ea');
+                const data = await response.json();
+                console.log(data);
+                setMessage(JSON.stringify(data));
+            } catch (e) {
+                console.error(e);
+                setMessage('Error fetching data');
+            }
+        };
+
+        fetchData();
+        // try {
+        // fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/')
+        // // .then(res => res.json())
+        // // .then(res => setMessage(res.description));
+        // // .then(res => res);
+        // } catch (e) {
+        // console.error(e);
+        // }
         
-    });
+    }, []);
 
     return (
         <main>
