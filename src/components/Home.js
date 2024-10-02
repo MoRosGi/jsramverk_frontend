50b692624e02bf5e0a300fdfacb63d10ef712f20
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-02 10:44:08 +0200

    Remove content element from list of documents

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,34 +5,34 @@ const Home = () => {
     const [documents, setDocuments] = useState([]); // Init as array inst of string
 
     useEffect(() => {
-        // const fetchData = async () => {
+        const fetchData = async () => {
             try {
-                // const response = await fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/documents');
-                const data = [
-                    {
-                        _id: '1',
-                        title: 'Placeholder title',
-                        content: 'Placeholder content'
-                    },
-                    {
-                        _id: '2',
-                        title: 'Placeholder title2',
-                        content: 'Placeholder content2'
-                    }
-                ];
-                // const data = await response.json();
+                const response = await fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/documents');
+                // const data = [
+                //     {
+                //         _id: '1',
+                //         title: 'Placeholder title',
+                //         content: 'Placeholder content'
+                //     },
+                //     {
+                //         _id: '2',
+                //         title: 'Placeholder title2',
+                //         content: 'Placeholder content2'
+                //     }
+                // ];
+                const data = await response.json();
                 console.log("Data", data);
                 
-                // setDocuments(data.data); // nested object
-                setDocuments(data); // nested object
-                console.log("Documents", documents);
+                setDocuments(data.data); // nested object
+                // setDocuments(data); // nested object
+                // console.log("Documents", documents);
             } catch (e) {
                 console.error(e);
                 setDocuments([{ error: 'Error fetching data'}]); // Adjust error for display instead of array
             }
-        // };
+        };
 
-        // fetchData();
+        fetchData();
         // try {
         // fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/')
         // // .then(res => res.json())
@@ -52,8 +52,7 @@ const Home = () => {
                     // Use _id as key prop, give unique id to let React track elements 
                     // when re-rendering virtual DOM
                     <div key={document._id}>
-                        <Link to={`/${document._id}`}><h2>{document.title}</h2></Link>
-                        <p>{document.content}</p>
+                        <h2><Link to={`/${document._id}`}>{document.title}</Link></h2>
                     </div>
                 ))
             ) : (
