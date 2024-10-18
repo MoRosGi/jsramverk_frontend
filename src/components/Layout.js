e2be4d2c61ff3ed5452d3ab7ff89f099385e2a02
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-18 13:46:46 +0200

    Add links to login and register

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,12 @@ const Layout = () => {
             <li>
                 <Link to="/documentform">Nytt dokument</Link>
             </li>
+            <li>
+                <Link to="/login">Logga in</Link>
+            </li>
+            <li>
+                <Link to="/register">Registrera</Link>
+            </li>
             </ul>
         </nav>
 
