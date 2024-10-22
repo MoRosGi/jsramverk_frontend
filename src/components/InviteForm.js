dfb4e64ed82ca775cc334b946a3c8765569cd8c1
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-22 11:24:38 +0200

    Add InviteForm

---
+++ src/components/InviteForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const InviteForm = ({ documentId }) => {
+
+    const [inviteEmail, setInviteEmail] = useState('');
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInviteEmail({ ...inviteEmail, [name]: value });
+        };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const response = await fetch('https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/invite', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({ documentId, inviteEmail })
+            });
+    
+            const result = await response.json();
+            const token = result.data.token;
+
+            sessionStorage.setItem('token', token);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                name="inviteEmail"
+                placeholder="Mottagarens e-postadress"
+                value={inviteEmail}
+                onChange={handleChange}
+                required
+            />
+            <button type="submit">Skicka</button>
+        </form>
+    );
+};
+
+export default InviteForm;
