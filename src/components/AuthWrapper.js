96f0f953da5b204df7749695396a582f11fac440
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-26 14:37:55 +0200

    Add useEffect hook

--- src/components/AuthWrapper.js
+++ src/components/AuthWrapper.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthWrapper = ({ children }) => {
     const navigate = useNavigate();
     const token = sessionStorage.getItem('token');
 
-    if (!token) {
-        navigate('/');
-        return null;
-    }
+    useEffect(() => {
+        if (!token) {
+            navigate('/');
+        }
+    }, [token, navigate]);
+    
+    if (!token) return null;
 
     return <>{ children }</>;
 }
