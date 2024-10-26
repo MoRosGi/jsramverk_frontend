fe5309b47a63ebddbd6070e5f056e2a03ca3477f
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-26 22:40:14 +0200

    Change behaviour of select
    - Add if-statement to check if selected receiver is '', and if it is, trigger toast message and early return.
    - Add option in select with value "", disabled, and text "Select a recipient" to display on render.
    - Remove placeholder parameter in <select />, it did nothing.

--- src/components/InviteForm.js
+++ src/components/InviteForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 
 const InviteForm = ({ documentId }) => {
+    // Add placeholder as default receiver to make it show on render
     const [inviteForm, setInviteForm] = useState({ receiver: '', documentId: documentId });
 
     const handleChange = (e) => {
@@ -11,6 +12,12 @@ const InviteForm = ({ documentId }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // Check select menu for placeholder, early return if no valid selection
+        if (inviteForm.receiver === '') {
+            toast('You must select a recipient to send an invite.');
+            return;
+        }
 
         console.log(inviteForm);
         console.log(typeof(inviteForm.documentId));
@@ -51,11 +58,11 @@ const InviteForm = ({ documentId }) => {
             </div>
             <select
                 name="receiver"
-                placeholder="Recipient's e-mail"
                 value={inviteForm.receiver}
                 onChange={handleChange}
                 required
             >
+                <option value="" disabled>Select a recipient</option>
                 <option value="annie.v.gustafsson@gmail.com">Annie Gustafsson</option>
                 <option value="morgane.rose.girard@gmail.com">Morgane Girard</option>
                 <option value="angt23@student.bth.se">Annie Student</option>
