43bfe48a419490aa2a6ea2ff39b3804c6c7f02c6
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-26 22:59:49 +0200

    Change router wrapping, add footer, add div
    - Move public routes outside Layout route to not render
    Layout component in public routes:
    	- LoginForm.js
    	- RegisterForm.js
    	- AcceptInvite.js
    
    - Keep protected routes inside Layout route to render
    Layout component in protected routes:
    	- Document.js
    	- DocumentEdit.js
    	- DocumentForm.js
    	- UserDocuments.js
    Effect: Menu options and LogoutButton are not rendered
    unless you are logged in, ie has token in session.
    
    - Import and add Footer component outside routes to display it in all routes.
    
    - Add <div className="app-container"> as wrapper for the app.

--- src/App.js
+++ src/App.js
@@ -10,23 +10,36 @@ import RegisterForm from "./components/RegisterForm.js";
 import LoginForm from "./components/LoginForm.js";
 import UserDocuments from "./components/UserDocuments.js";
 import AcceptInvite from "./components/AcceptInvite.js";
+import Footer from "./components/Footer.js";
 
-
+// Changes 2024-10-26:
+// Divide routes into public and protected to display Layout, including menu and 
+// LogoutButton, only on protected routes.
+// Include Footer component inside HashRouter, outside of the Routes to include it at
+// the bottom of every route.
+// Add className "app-container", a flexbox wrapper to simplify styling and help 
+// position the Footer component.
 function App() {
   return (
     <HashRouter>
-    <h1>SSR-Editor</h1>
-      <Routes>
-        <Route path="/" element={<Layout />}>
+      <div className="app-container">
+        <h1>SSR-Editor</h1>
+        <Routes>
+          {/* Public routes */}
           <Route index element={<LoginForm />} />
-          <Route path="/:id" element={<Document />}/>
-          <Route path="/documentform" element={<DocumentForm />}/>
-          <Route path="/documentedit/:id" element={<DocumentEdit />}/>
           <Route path="/register" element={<RegisterForm />}/>
-          <Route path="/userdocuments" element={<UserDocuments />}/>
           <Route path="/invite/:inviteId" element={<AcceptInvite />}/>
-        </Route>
-      </Routes>
+
+          {/* Protected routes */}
+          <Route path="/" element={<Layout />}>
+            <Route path="/:id" element={<Document />}/>
+            <Route path="/documentform" element={<DocumentForm />}/>
+            <Route path="/documentedit/:id" element={<DocumentEdit />}/>
+            <Route path="/userdocuments" element={<UserDocuments />}/>
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
     </HashRouter>
   );
 }
