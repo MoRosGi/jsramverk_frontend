20cd24d420049b5d428bb9bbd2b9fcba9868c2ab
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-29 18:06:35 +0100

    Add className

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
+import styles from "./Layout.module.css";
 
 const Layout = () => {
     return (
         <>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Log in/Register</Link>
+                <ul className={styles.ul}>
+                    <li className={styles.li}>
+                        <Link to="/" className={styles.link}>Log in/Register</Link>
                     </li>
                     <li>
-                        <Link to="/userdocuments">My documents</Link>
+                        <Link to="/userdocuments"className={styles.link}>My documents</Link>
                     </li>
                     <li>
-                        <Link to="/documentform">New document</Link>
+                        <Link to="/documentform"className={styles.link}>New document</Link>
                     </li>
                     <li>
                         <LogoutButton />
