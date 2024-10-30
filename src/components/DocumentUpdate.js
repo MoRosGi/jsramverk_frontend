54370f5d3d2ec1c911b5834c0f40710dbb43ef4e
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-10-30 13:13:57 +0100

    Updates to socket functionality
    - Add toast on document save
    - Navigate to DocumentEdit if socket not available
    - Reactivate fetch data
    - Add style import

--- src/components/DocumentUpdate.js
+++ src/components/DocumentUpdate.js
@@ -1,85 +1,77 @@
 import React, { useEffect, useState, useRef } from 'react';
-// import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { useParams } from 'react-router-dom';
 import InviteForm from './InviteForm';
 import AuthWrapper from './AuthWrapper';
 import { toast } from 'react-toastify';
 import { io } from 'socket.io-client';
+import styles from './DocumentUpdate.module.css';
 
-// const SERVER_URL = 'https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net';
-const SERVER_URL ='http://localhost:1337';
-// let socket; // Move let socket outside function to make it last
+const SERVER_URL = 'https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net';
+// const SERVER_URL ='http://localhost:1337';
 
 const DocumentUpdate = () => {
     const { id: _id } = useParams()
-    // const titleRef = useRef('');
     const [documentUpdate, setDocumentUpdate] = useState({ title: '', content: '' });
-    // const [title, setTitle] = useState('');
-    // const [content, setContent] = useState('');
-    // const navigate = useNavigate();
-    // console.log("First", _id);
-    // console.log(useParams());
+    const navigate = useNavigate();
     const socket = useRef(null);
+    let toastId = useRef(null);
 
     useEffect(() => {
 
-        // const fetchData = async () => {
-        //     try {
-        //         const response = await fetch(
-        //             `https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/documents/${_id}`,
-        //             {
-        //                 method: 'GET',
-        //                 headers: {
-        //                     'x-access-token': sessionStorage.getItem("token")
-        //                 }
-        //             }
-        //         );
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `https://jsramverk-text-editor-beb8fuhxangpdqfh.northeurope-01.azurewebsites.net/documents/${_id}`,
+                    {
+                        method: 'GET',
+                        headers: {
+                            'x-access-token': sessionStorage.getItem("token")
+                        }
+                    }
+                );
 
-        //         const data = await response.json();
-        //         setDocumentUpdate(data.data);
-        //     } catch (e) {
-        //         toast(e);
-        //         console.error(e);
-        //     }
-        // };
+                const data = await response.json();
+                setDocumentUpdate(data.data);
+            } catch (e) {
+                toast(e);
+                console.error(e);
+            }
+        };
 
-        // fetchData();
-        // socket.current = io(SERVER_URL);
+        fetchData();
         socket.current = io(SERVER_URL, { query: {token: sessionStorage.getItem('token')}});
-        console.log("Token:", sessionStorage.getItem('token'));
+        console.log(socket.current);
+
+        if (socket.current._opts.secure === false) {
+            navigate(`/documentedit/${_id}`);
+        }
 
         socket.current.emit("joinDocument", _id);
-        // console.log("After join room", _id);
-
-        // socket.current.on(`userInfo`, (data) => {
-        //     console.log(`userInfo`, data.user);
-        // })
 
         socket.current.on("documentUpdate", (data) => {
             setDocumentUpdate(data);
-            // console.log("Update data:", data);
         });
 
         socket.current.on("documentSaved", (data) => {
-            toast(data.message);
+
+            if (!toastId.current) {
+                toastId.current = toast(data.message, { autoClose: 5000});
+            } else if (toast.isActive(toastId.current)) {
+                toast.update(toastId.current, { render: data.message,  autoClose: 5000});
+            } else {
+                toastId.current = toast(data.message, { autoClose: 5000});
+            }
         });
 
         return () => {
             socket.current.disconnect();
         }
-    }, [_id]);
-
-    // const handleChange = (e) => {
-    //     const value = e.target;
-
-    //     socket.emit("content", value);
-    // }
+    }, [_id, navigate]);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         const updatedDocument = { ...documentUpdate, [name]: value };
-        // console.log(e.target);
-        // console.log("Title:", content.title);
         setDocumentUpdate(updatedDocument);
 
         socket.current.emit("documentUpdate", {
@@ -87,40 +79,44 @@ const DocumentUpdate = () => {
             title: updatedDocument.title,
             content: updatedDocument.content
         });
-        // console.log("New title", updatedDocument.title);
     };
 
     return (
         <>
             <AuthWrapper>
+            <InviteForm _id={_id} />
                 <div>
-                    <div>
-                        <label htmlFor='title'>Title:</label>
+                    <div className={styles.updatewrapper}>
+                        <div className={styles.editorwrapper}>
+                            <div>
+                                <label htmlFor='title'>Title:</label>
+                            </div>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                value={documentUpdate.title}
+                                onChange={handleChange}
+                            />
+                            <div>
+                                <div>
+                                    <label htmlFor='content'>Content:</label>
+                                </div>
+                                <textarea
+                                    name="content"
+                                    id="content"
+                                    value={documentUpdate.content}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            </div>
+                        
+                        <div id="output-container">
+                            <h1>{documentUpdate.title}</h1>
+                            <p>{documentUpdate.content}</p>
+                        </div>
+                        </div>
                     </div>
-                    <input
-                        type="text"
-                        name="title"
-                        id="title"
-                        value={documentUpdate.title}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <div>
-                        <label htmlFor='content'>Content:</label>
-                    </div>
-                    <textarea
-                        name="content"
-                        id="content"
-                        value={documentUpdate.content}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div id="output-container">
-                    <h1>{documentUpdate.title}</h1>
-                    <p>{documentUpdate.content}</p>
-                </div>
-                <InviteForm _id={_id} />
             </AuthWrapper>
         </>
     );
